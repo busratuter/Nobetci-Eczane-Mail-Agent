12a51b200b95cd984973d1989dbcf2bab32aad0f
busratuter <busratuter@outlook.com>
2025-06-20 23:53:34 +0300

    added model info

--- agent.py
+++ agent.py
@@ -1,10 +1,15 @@
 from agno.agent import Agent
+from agno.models.openai import OpenAIChat
 from tools import nobetci_eczane_cek, mail_gonder
+
+default_model = OpenAIChat() # default model gpt-4o
 
 tools = [nobetci_eczane_cek, mail_gonder]
 
 agent = Agent(
     name="NobetciEczaneAgent",
     description="Belirtilen il ve ilçedeki nöbetçi eczaneleri çekip Outlook ile e-posta gönderen agent.",
-    tools=tools
+    tools=tools,
+    model=default_model,
+    markdown=True
 )
